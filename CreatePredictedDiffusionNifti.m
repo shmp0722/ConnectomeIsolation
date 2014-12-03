072723004d300f397df55640919142f6a2643539
Brian Wandell <wandell@stanford.edu>
2014-12-03 10:31:42 -0800

    Edits with SO for clarity and to loop

--- CreatePredictedDiffusionNifti.m
+++ CreatePredictedDiffusionNifti.m
@@ -1,10 +1,18 @@
 function CreatePredictedDiffusionNifti
 %% Use Franco's example data
-cd ~/Downloads/data
-addpath(genpath(pwd))
+
+try, lifeDemoDataPath
+catch
+    error('Add life data to your path');
+end
+
 
 %% Build the file names for the diffusion data, the anatomical MRI.
+
+% Here are the original dMRI data
 dwiFile       = fullfile(lifeDemoDataPath('diffusion'),'life_demo_scan1_subject1_b2000_150dirs_stanford.nii.gz');
+
+
 dwiFileRepeat = fullfile(lifeDemoDataPath('diffusion'),'life_demo_scan2_subject1_b2000_150dirs_stanford.nii.gz');
 t1File        = fullfile(lifeDemoDataPath('anatomy'),  'life_demo_anatomy_t1w_stanford.nii.gz');
 
@@ -17,7 +25,9 @@ bvals =   dlmread(fullfile(lifeDemoDataPath('diffusion'),'life_demo_scan1_subjec
 dwi   = dwiCreate('nifti',nifti,'bvecs',bvecs,'bvals',bvals);
 
 clear bvecs, clear bvals;
-%% 
+%% Take the first 10 fibers from the fiber group
+
+% This is intended to run quickly
 fgFileName    = fullfile(lifeDemoDataPath('tractography'), ...
                 'life_demo_mrtrix_csd_lmax10_probabilistic.mat');
 
@@ -27,6 +37,51 @@ small_fg.pathwayInfo = fg.pathwayInfo(1:10);
 % fgWrite(small_fg, fullfile(lifeDemoDataPath('diffusion'), 'small_fg.mat'));
 
 clear fg
+
+% small_fg.fibers{ii} are the coordinates in ACPC space
+
+%% We need the weights from the LiFE solution
+
+
+%% Build a model of the connectome.
+%
+% Running with the issue2 branch on LiFE
+
+feFileName    = 'life_build_model_demo_CSD_PROB_small';
+fe = feConnectomeInit(dwiFile, small_fg, feFileName,fullfile(fileparts(fgFileName)),dwiFileRepeat,t1File);
+%fe = feConnectomeInit(dwiFile, fgFileName, feFileName,fullfile(fileparts(fgFileName)),dwiFileRepeat,t1File);
+
+%% Fit the model with global weights.
+fe = feSet(fe,'fit',feFitModel(feGet(fe,'mfiber'),feGet(fe,'dsigdemeaned'),'bbnnls'));
+
+wgts = feGet(fe,'fiber weights');
+coords = 
+
+
+%% Make an empty nifti file the same size as the original
+pNifti = niftiCreate;
+pData = zeros(size(nifti.data));
+
+%% Compute the diffusion tensors for each node in each fiber
+
+Q = feComputeCanonicalDiffusion(fe.fg.fibers, [1 0 0]); % Q =voxTensors;
+
+
+%% Add diffusion signal for each fiber coordinate
+oneFiber = floor(fe.fg.fibers{1});
+
+for ii=1:length(oneFiber)
+    for jj=1:length(bvec)
+        pData(oneFiber(1,1),oneFiber(2,1),oneFiber(3,1),jj) = ...
+            wgts(ii)*exp(-b*(bvec(jj)'*Q*bvec(jj))); 
+    end
+end
+
+
+pNifti = niftiSet(pNifti,'data',pData);
+
+%% For each 
+
 %% 
 feFileName    = 'life_build_model_demo_CSD_PROB_small';
 
@@ -72,5 +127,7 @@ voxTensors        = feGet(fe,'voxeltensors',usedVoxels);
 Q = feComputeCanonicalDiffusion(small_fg.fibers, [1 0 0]); % Q =voxTensors;
 
 voxDSig = feComputeSignal(S0, bvecs, bvals, Q);
+
+
 
 
